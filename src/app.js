0043d845dc688f45e6cfb7c317ddffcf695522d6
chaitanya-annamreddy <achaitanya0505@gmail.com>
2025-03-08 20:31:12 +0530

    get api

--- src/app.js
+++ src/app.js
@@ -3,23 +3,26 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json()); // This middleware parses incoming requests with JSON payloads for all the routes
+
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Virat",
-    lastName: "Kohli",
-    email: "virat@gmail.com",
-    password: "virat@123",
-    age: 38,
-    gender: "male",
-  };
-
-  const user = new User(userObj); // creating a new user with userObj or creating a new instance of the User model
-
+  const user = new User(req.body); // we created a new user with req.body or we can say we created a new instance of the User model
+  // whoever sends the post request to the /signup route, a new user instance is created
+  // now we will save this user to the database
   try {
-    await user.save(); // saving the user to the database
+    await user.save(); // saving the user to the database, also we can say document is saved to the database
     res.send("User added successfully");
   } catch (err) {
     res.status(400).send("Error adding user:", err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Error retrieving users:", err.message);
   }
 });
 
